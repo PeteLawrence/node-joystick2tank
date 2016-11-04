3192e05e863f93fc261afc7bdd6b13e9557a9fd7
Pete Lawrence <petejlawrence@icloud.com>
2016-11-04 18:36:48 +0000

    Fixed require'ing Allow passing of options Refactored to support more efficient handling of options

--- README.md
+++ README.md
@@ -11,6 +11,13 @@ var values = j2t(-30, 100); // { left: 70, right: 100 }
 
 See example.js to see how to handle scaling values from joysticks that don't operate from -100 to 100.
 
+### Options
+You can pass in an options object as the 3rd parameter.  Valid values are:
+```invertX``` Invert the X axis
+```invertY``` Invert the Y axis
+```maxX```    The maximum value of X. Defaults to 100
+```maxY```    The maximum value of Y. Defaults to 100
+
 
 ### Credits
 Based on the excellent [write-up by Mauser](http://home.kendra.com/mauser/Joystick.html).
--- src/index.js
+++ src/index.js
@@ -1,34 +1,63 @@
-module.exports = function(x, y) {
+var defaults = require('defaults');
 
-  //Check that X is between -100 and 100
-  if (x > 100 || x < -100) {
-    throw new RangeError('X must be between -100 and 100');
-  }
+var Joystick2Tank = function(opts) {
+  var module = {};
 
-  //Check that Y is between -100 and 100
-  if (y > 100 || y < -100) {
-    throw new RangeError('Y must be between -100 and 100');
-  }
-
-  //Invert x
-  x = x * -1;
-
-  //Calculate R+L (Call it V): V =(100-ABS(X)) * (Y/100) + Y
-  var v = (100 - Math.abs(x)) * y/100 + y
-
-  //Calculate R-L (Call it W): W= (100-ABS(Y)) * (X/100) + X
-  var w = (100 - Math.abs(y)) * x/100 + x;
-
-  //Calculate R: R = (V+W) /2
-  var r = (v + w)/2;
-
-  //Calculate L: L= (V-W)/2
-  var l = (v - w)/2;
-
-
-  return {
-    left: l,
-    right: r
+  module.getDefaults = function()
+  {
+    return {
+      invertX: false,
+      invertY: true,
+      maxX: 100,
+      maxY: 100
+    };
   };
 
+
+  module.convert = function(x, y) {
+    //Handle options
+    if (module.options.invertX) {
+      x = x * -1;
+    }
+    if (module.options.invertY) {
+      y = y * -1;
+    }
+
+    //Check that X is between -100 and 100
+    if (x > 100 || x < -100) {
+      throw new RangeError('X must be between -100 and 100');
+    }
+
+    //Check that Y is between -100 and 100
+    if (y > 100 || y < -100) {
+      throw new RangeError('Y must be between -100 and 100');
+    }
+
+    //Invert x
+    x = x * -1;
+
+    //Calculate R+L (Call it V): V =(100-ABS(X)) * (Y/100) + Y
+    var v = (100 - Math.abs(x)) * y/100 + y
+
+    //Calculate R-L (Call it W): W= (100-ABS(Y)) * (X/100) + X
+    var w = (100 - Math.abs(y)) * x/100 + x;
+
+    //Calculate R: R = (V+W) /2
+    var r = (v + w)/2;
+
+    //Calculate L: L= (V-W)/2
+    var l = (v - w)/2;
+
+
+    return {
+      left: l,
+      right: r
+    };
+  }
+
+  module.options = defaults(opts, module.getDefaults());
+
+  return module;
 }
+
+module.exports = Joystick2Tank;
